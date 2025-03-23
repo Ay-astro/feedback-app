b1b5fab1e3a77f3d546380d468bdbea5369644d9
Ay-astro <g.abdullahi.yusuf@gmail.com>
2025-03-23 21:14:55 +0100

    fix the issue with the submit

--- src/component/FeedbackForm.jsx
+++ src/component/FeedbackForm.jsx
@@ -32,7 +32,7 @@ function FeedbackForm() {
     
     const handleSubmit = (e)=>{
         e.preventDefault()
-        if(text.trim().length >10){
+        if(text.trim().length >=10){
         const newFeedback = {
             rating,
             text
